793b5550d5cb6a401edeffc06bb78999970789e6
Martín <tinchobeltrame@gmail.com>
2018-08-21 10:30:34 -0300

    Se realiza el algoritmo de fibonacci

--- fibo/src/main/java/ar/edu/untref/dyasc/Program.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -2,10 +2,26 @@ package ar.edu.untref.dyasc;
 
 public class Program {
 
-    public static void main(String[] args) {
-    
-        System.out.println("Hola a todos!!!");
-    
-    }
+	private static int fibonacciDeUnNumero(int numero) {
+		if (numero <= 1) {
+			return 1;
+		} else {
+			return fibonacciDeUnNumero(numero - 1) + fibonacciDeUnNumero(numero - 2);
+		}
+	}
 
+	private static void mostrarSerieFibonacci(int numero) {
+		System.out.print("fibo<" + numero + ">:");
+		for (int i = 1; i <= numero; i++) {
+			int resultado = fibonacciDeUnNumero(i);
+			System.out.print(" " + resultado);
+		}
+	}
+
+	public static void main(String[] args) {
+		Integer parametro = Integer.valueOf(args[0]);
+		if (parametro != null) {
+			mostrarSerieFibonacci(parametro);
+		}
+	}
 }
