746a89489c9cab3b33ca8edc6d1813ff945a669a
Martín <tinchobeltrame@gmail.com>
2018-09-09 19:09:43 -0300

    Se agrega el registro de suscripcion a la libreria

--- libreria/src/main/java/ar/edu/untref/dyasc/dominio/Libreria.java
+++ libreria/src/main/java/ar/edu/untref/dyasc/dominio/Libreria.java
@@ -3,6 +3,7 @@ package ar.edu.untref.dyasc.dominio;
 public class Libreria {
 	
 	private RegistroVentas registroVentas;
+	private RegistroSuscripcion registroSuscripciones;
 
 	public Libreria(RegistroVentas registroVentas) {
 		this.registroVentas = registroVentas;
@@ -10,5 +11,9 @@ public class Libreria {
 
 	public void realizarVenta(Venta venta) {
 		registroVentas.registrar(venta);
+	}
+
+	public void realizarSuscripcion(Suscripcion suscripcion) {
+		registroSuscripciones.registrar(suscripcion);
 	}
 }
--- libreria/src/test/java/ar/edu/untref/dyasc/LibreriaDebe.java
+++ libreria/src/test/java/ar/edu/untref/dyasc/LibreriaDebe.java
@@ -11,7 +11,9 @@ import ar.edu.untref.dyasc.dominio.Cliente;
 import ar.edu.untref.dyasc.dominio.Libreria;
 import ar.edu.untref.dyasc.dominio.Mes;
 import ar.edu.untref.dyasc.dominio.Producto;
+import ar.edu.untref.dyasc.dominio.RegistroSuscripcion;
 import ar.edu.untref.dyasc.dominio.RegistroVentas;
+import ar.edu.untref.dyasc.dominio.Suscripcion;
 import ar.edu.untref.dyasc.dominio.Venta;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,12 +22,16 @@ public class LibreriaDebe {
 	private Producto PRODUCTO = new Producto(100.0);
 	private Mes ENERO = Mes.ENERO;
 	private Cliente CLIENTE = new Cliente("Jorge", "Rich", "Av. Siempreviva", 1123);
+
 	private Venta VENTA = new Venta(PRODUCTO, ENERO, CLIENTE);
+	private Suscripcion SUSCRIPCION = new Suscripcion();
 
 	private Libreria libreria;
 
 	@Mock
 	RegistroVentas registroVentas;
+	@Mock
+	RegistroSuscripcion registroSuscripciones;
 
 	@Before
 	public void inicializar() {
@@ -38,5 +44,13 @@ public class LibreriaDebe {
 		libreria.realizarVenta(VENTA);
 
 		Mockito.verify(registroVentas).registrar(VENTA);
+	}
+
+	@Test
+	public void registrar_una_suscripcion() {
+
+		libreria.realizarSuscripcion(SUSCRIPCION);
+
+		Mockito.verify(registroSuscripciones).registrar(SUSCRIPCION);
 	}
 }
