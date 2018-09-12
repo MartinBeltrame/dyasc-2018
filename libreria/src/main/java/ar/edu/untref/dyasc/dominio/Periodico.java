456afea6998516949c3e359f435905ee23298bc3
Martín <tinchobeltrame@gmail.com>
2018-09-12 17:02:21 -0300

    Se añade el atributo de periodicidad a la revista y el periodico

--- libreria/src/main/java/ar/edu/untref/dyasc/dominio/Periodico.java
+++ libreria/src/main/java/ar/edu/untref/dyasc/dominio/Periodico.java
@@ -2,7 +2,17 @@ package ar.edu.untref.dyasc.dominio;
 
 public class Periodico extends Producto {
 
+	private int periodicidad;
+
 	public Periodico(Double precio) {
 		super(precio);
+	}
+
+	public int getPeriodicidad() {
+		return periodicidad;
+	}
+
+	public void setPeriodicidad(int periodicidad) {
+		this.periodicidad = periodicidad;
 	}
 }
--- libreria/src/main/java/ar/edu/untref/dyasc/dominio/Revista.java
+++ libreria/src/main/java/ar/edu/untref/dyasc/dominio/Revista.java
@@ -2,7 +2,17 @@ package ar.edu.untref.dyasc.dominio;
 
 public class Revista extends Producto {
 
+	private int periodicidad;
+	
 	public Revista(Double precio) {
 		super(precio);
+	}
+	
+	public void setPeriodicidad(int periodicidad) {
+		this.periodicidad = periodicidad;
+	}
+	
+	public int getPeriodicidad() {
+		return periodicidad;
 	}
 }
