d2ff6cc57a30511cab35d09531b6decaccef2264
Martín <tinchobeltrame@gmail.com>
2018-09-25 15:18:32 -0300

    Se agregan test para la clase Operaciones.sumatoria

--- fibo3/src/test/java/ar/edu/untref/dyasc/OperacionesDebe.java
+++ fibo3/src/test/java/ar/edu/untref/dyasc/OperacionesDebe.java
@@ -8,6 +8,11 @@ import ar.edu.untref.dyasc.dominio.Operaciones;
 
 public class OperacionesDebe {
 
+	private int[] SERIE_DE_CERO = { 0 };
+	private int[] SERIE_DE_DOS = { 0, 1, 1 };
+	private int[] SERIE_DE_TRES = { 0, 1, 1, 2 };
+	private int[] SERIE_DE_CINCO = { 0, 1, 1, 2, 3, 5, 8 };
+
 	private Operaciones operaciones;
 
 	@Before
@@ -18,9 +23,7 @@ public class OperacionesDebe {
 	@Test
 	public void devolver_un_array_con_el_valor_cero_para_la_serie_invertida_de_cero() {
 
-		int[] serieDeCero = new int[1];
-
-		int[] obtenido = operaciones.invertirSerie(serieDeCero);
+		int[] obtenido = operaciones.invertirSerie(SERIE_DE_CERO);
 
 		int[] esperado = { 0 };
 		Assert.assertArrayEquals(esperado, obtenido);
@@ -29,9 +32,7 @@ public class OperacionesDebe {
 	@Test
 	public void devolver_un_array_con_los_valores_invertidos_de_la_serie_de_dos() {
 
-		int[] serieDeCero = { 0, 1, 1 };
-
-		int[] obtenido = operaciones.invertirSerie(serieDeCero);
+		int[] obtenido = operaciones.invertirSerie(SERIE_DE_DOS);
 
 		int[] esperado = { 1, 1, 0 };
 		Assert.assertArrayEquals(esperado, obtenido);
@@ -40,12 +41,33 @@ public class OperacionesDebe {
 	@Test
 	public void devolver_un_array_con_los_valores_invertidos_de_la_serie_de_cinco() {
 
-		int[] serieDeCero = { 0, 1, 1, 2, 3, 5, 8 };
-
-		int[] obtenido = operaciones.invertirSerie(serieDeCero);
+		int[] obtenido = operaciones.invertirSerie(SERIE_DE_CINCO);
 
 		int[] esperado = { 8, 5, 3, 2, 1, 1, 0 };
 		Assert.assertArrayEquals(esperado, obtenido);
 	}
 
+	@Test
+	public void devolver_la_sumatoria_de_la_serie_de_cero() {
+
+		int obtenido = operaciones.sumatoria(SERIE_DE_CERO);
+
+		Assert.assertEquals(0, obtenido);
+	}
+
+	@Test
+	public void devolver_la_sumatoria_de_la_serie_de_tres() {
+
+		int obtenido = operaciones.sumatoria(SERIE_DE_TRES);
+
+		Assert.assertEquals(4, obtenido);
+	}
+	
+	@Test
+	public void devolver_la_sumatoria_de_la_serie_de_cinco() {
+
+		int obtenido = operaciones.sumatoria(SERIE_DE_CINCO);
+
+		Assert.assertEquals(20, obtenido);
+	}
 }
