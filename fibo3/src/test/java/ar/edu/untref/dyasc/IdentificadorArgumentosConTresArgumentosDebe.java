92ba4976810fab5b5ebd9802c6ddaea75b2c126e
Martín <tinchobeltrame@gmail.com>
2018-09-26 11:04:06 -0300

    Se añaden test al identificador con tres argumentos

--- fibo3/src/test/java/ar/edu/untref/dyasc/IdentificadorArgumentosConTresArgumentosDebe.java
+++ fibo3/src/test/java/ar/edu/untref/dyasc/IdentificadorArgumentosConTresArgumentosDebe.java
@@ -9,11 +9,19 @@ import ar.edu.untref.dyasc.utilidades.Constantes;
 
 public class IdentificadorArgumentosConTresArgumentosDebe {
 
+	// Modo sumatoria
 	private String[] TRES_ARGUMENTOS_CON_OPCION_HDS = new String[] { "-o=hd", "-m=s", "5" };
 	private String[] TRES_ARGUMENTOS_CON_OPCION_HIS = new String[] { "-o=hi", "-m=s", "5" };
 	private String[] TRES_ARGUMENTOS_CON_OPCION_VDS = new String[] { "-o=vd", "-m=s", "5" };
 	private String[] TRES_ARGUMENTOS_CON_OPCION_VIS = new String[] { "-o=vi", "-m=s", "5" };
 
+	// Modo lista
+	private String[] TRES_ARGUMENTOS_CON_OPCION_HDL = new String[] { "-o=hd", "-m=l", "5" };
+	private String[] TRES_ARGUMENTOS_CON_OPCION_HIL = new String[] { "-o=hi", "-m=l", "5" };
+	private String[] TRES_ARGUMENTOS_CON_OPCION_VDL = new String[] { "-o=vd", "-m=l", "5" };
+	private String[] TRES_ARGUMENTOS_CON_OPCION_VIL = new String[] { "-o=vi", "-m=l", "5" };
+
+	// Modo archivo
 	private String[] TRES_ARGUMENTOS_CON_OPCION_HD_Y_MODO_ARCHIVO = new String[] { "-o=hd", "-f=archivo.txt", "5" };
 	private String[] TRES_ARGUMENTOS_CON_OPCION_HI_Y_MODO_ARCHIVO = new String[] { "-o=hi", "-f=archivo.txt", "5" };
 	private String[] TRES_ARGUMENTOS_CON_OPCION_VD_Y_MODO_ARCHIVO = new String[] { "-o=vd", "-f=archivo.txt", "5" };
@@ -67,6 +75,49 @@ public class IdentificadorArgumentosConTresArgumentosDebe {
 		String modo = resultado[1];
 
 		Assert.assertEquals(Constantes.OPCION_VIS, opcion);
+		Assert.assertEquals(Constantes.MODO_PANTALLA, modo);
+	}
+
+	public void identificar_los_argumentos_con_opcion_horizontal_directa_y_modo_lista() {
+
+		String[] resultado = identificadorArgumentos.identificar(TRES_ARGUMENTOS_CON_OPCION_HDL);
+		String opcion = resultado[0];
+		String modo = resultado[1];
+
+		Assert.assertEquals(Constantes.OPCION_HD, opcion);
+		Assert.assertEquals(Constantes.MODO_PANTALLA, modo);
+	}
+
+	@Test
+	public void identificar_los_argumentos_con_opcion_horizontal_inversa_y_modo_lista() {
+
+		String[] resultado = identificadorArgumentos.identificar(TRES_ARGUMENTOS_CON_OPCION_HIL);
+		String opcion = resultado[0];
+		String modo = resultado[1];
+
+		Assert.assertEquals(Constantes.OPCION_HI, opcion);
+		Assert.assertEquals(Constantes.MODO_PANTALLA, modo);
+	}
+
+	@Test
+	public void identificar_los_argumentos_con_opcion_vertical_directa_y_modo_lista() {
+
+		String[] resultado = identificadorArgumentos.identificar(TRES_ARGUMENTOS_CON_OPCION_VDL);
+		String opcion = resultado[0];
+		String modo = resultado[1];
+
+		Assert.assertEquals(Constantes.OPCION_VD, opcion);
+		Assert.assertEquals(Constantes.MODO_PANTALLA, modo);
+	}
+
+	@Test
+	public void identificar_los_argumentos_con_opcion_vertical_inversa_y_modo_lista() {
+
+		String[] resultado = identificadorArgumentos.identificar(TRES_ARGUMENTOS_CON_OPCION_VIL);
+		String opcion = resultado[0];
+		String modo = resultado[1];
+
+		Assert.assertEquals(Constantes.OPCION_VI, opcion);
 		Assert.assertEquals(Constantes.MODO_PANTALLA, modo);
 	}
 
